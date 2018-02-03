daf7147c2d27313e63b2a435da2e80177c4e332d
Tjaart Blignaut <tjaart@getbucks.com>
2018-02-04 00:28:24 +0200

    Add grpc datetime converter

---
+++ MyBucks.Core.ApiGateway.ApiClient/TypeConverters/GrpcDateTimeConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using Google.Protobuf.WellKnownTypes;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Type = System.Type;
+
+namespace MyBucks.Core.ApiGateway.ApiClient.TypeConverters
+{
+    public class GrpcDateTimeConverter : JsonConverter
+    {
+        public class ProtoTimeStamp
+        {
+
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var val = (DateTime)value;
+            var ts = (val.ToUniversalTime().ToTimestamp());
+
+            writer.WriteStartObject();
+            writer.WritePropertyName("seconds");
+            serializer.Serialize(writer, ts.Seconds);
+            writer.WritePropertyName("nanos");
+            serializer.Serialize(writer, ts.Nanos);
+            writer.WriteEndObject();
+
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            JObject jsonObject = JObject.Load(reader);
+            var properties = jsonObject.Properties().ToList();
+            var ts = new Timestamp();
+            ts.Seconds = (long)properties[0].Value;
+            ts.Nanos = (int)properties[1].Value;
+            return ts.ToDateTime();
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(DateTime).IsAssignableFrom(objectType);
+        }
+    }
+}
