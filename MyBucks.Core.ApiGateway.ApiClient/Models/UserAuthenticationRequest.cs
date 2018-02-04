cd83d6b586029daf2016b7e44b9be8e626eddb34
Tjaart Blignaut <tjaart@getbucks.com>
2018-02-04 21:14:06 +0200

    fix property name serialization

--- MyBucks.Core.ApiGateway.ApiClient/Models/UserAuthenticationRequest.cs
+++ MyBucks.Core.ApiGateway.ApiClient/Models/UserAuthenticationRequest.cs
@@ -1,10 +1,19 @@
-﻿namespace MyBucks.Core.ApiGateway.ApiClient.Models
+﻿using Newtonsoft.Json;
+
+namespace MyBucks.Core.ApiGateway.ApiClient.Models
 {
     public class UserAuthenticationRequest
     {
+        [JsonProperty("context")]
         public string Context { get; set; }
+        
+        [JsonProperty("email")]
         public string Email { get; set; }
+        
+        [JsonProperty("mobile_number")]
         public string MobileNumber { get; set; }
+        
+        [JsonProperty("password")]
         public string Password { get; set; }
     }
 }
