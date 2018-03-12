12a916208a3c563ab9f07b47fea649f093e4cea7
Kerry Trentham <kerry@mybucks.com>
2018-03-12 13:03:47 +0200

    Add gitignore, added default fincloud.getbucks.com host header, added constructor to allow modification of headers

--- MyBucks.Core.ApiGateway.ApiClient/Models/TokenAuthenticationCredentials.cs
+++ MyBucks.Core.ApiGateway.ApiClient/Models/TokenAuthenticationCredentials.cs
@@ -1,9 +1,11 @@
-﻿namespace MyBucks.Core.ApiGateway.ApiClient.Models
+﻿using System.Collections.Generic;
+
+namespace MyBucks.Core.ApiGateway.ApiClient.Models
 {
     public class TokenAuthenticationCredentials
     {
         public string ClientId { get; set; }
         public string ClientSecret { get; set; }
-        public string AppName { get; set; }
-    }
+		public string AppName { get; set; }
+	}
 }
--- MyBucks.Core.ApiGateway.ApiClient/MyBucksApiClient.cs
+++ MyBucks.Core.ApiGateway.ApiClient/MyBucksApiClient.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Flurl;
 using Flurl.Http;
@@ -10,28 +11,43 @@ namespace MyBucks.Core.ApiGateway.ApiClient
     {
         private readonly TokenAuthenticationCredentials _tokenAuthenticationCredentials;
 
-        private readonly string _context;
-        private BearerToken _tokenCollection;
+		private readonly string _context;
+		private BearerToken _tokenCollection;
         private readonly string _baseUrl;
 
-        public MyBucksApiClient(string baseUrl, TokenAuthenticationCredentials tokenAuthenticationCredentials, string context)
-        {
-            _context = context;
-            _tokenAuthenticationCredentials = tokenAuthenticationCredentials;
-            _baseUrl = baseUrl;
-        }
+		private Dictionary<string, string> _headers { get; set; }
 
-        public void SetToken(BearerToken existingToken)
+		public MyBucksApiClient(string baseUrl, TokenAuthenticationCredentials tokenAuthenticationCredentials, string context, Dictionary<string, string> headers)
+		{
+			_context = context;
+			_tokenAuthenticationCredentials = tokenAuthenticationCredentials;
+			_baseUrl = baseUrl;
+			_headers = headers;
+		}
+
+		public MyBucksApiClient(string baseUrl, TokenAuthenticationCredentials tokenAuthenticationCredentials, string context)
+		{
+			_context = context;
+			_tokenAuthenticationCredentials = tokenAuthenticationCredentials;
+			_baseUrl = baseUrl;
+		}
+
+		public void SetToken(BearerToken existingToken)
         {
             _tokenCollection = existingToken;
         }
 
         public async Task<BearerToken> RefreshToken()
         {
-            var result = await _baseUrl
+			if (_headers == null) _headers = new Dictionary<string, string>();
+
+			if (!_headers.ContainsKey("Host")) _headers.Add("Host", "fincloud.getbucks.com");
+
+			var result = await _baseUrl
                 .AppendPathSegment("tokens")
                 .AppendPathSegment(_tokenCollection.RefreshToken)
-                .WithBasicAuth(_tokenAuthenticationCredentials.ClientId, _tokenAuthenticationCredentials.ClientSecret)
+				.WithHeaders(_headers)
+				.WithBasicAuth(_tokenAuthenticationCredentials.ClientId, _tokenAuthenticationCredentials.ClientSecret)
                 .PostJsonAsync(new {context = _context}).ReceiveJson<BearerToken>();
             _tokenCollection = result;
             return result;
@@ -39,7 +55,11 @@ namespace MyBucks.Core.ApiGateway.ApiClient
 
         public async Task<BearerToken> GetAuthToken(string email, string password)
         {
-            var accountModel = new UserAuthenticationRequest
+			if (_headers == null) _headers = new Dictionary<string, string>();
+
+			if (!_headers.ContainsKey("Host")) _headers.Add("Host", "fincloud.getbucks.com");
+
+			var accountModel = new UserAuthenticationRequest
             {
                 Context = _context,
                 Email = email,
@@ -48,6 +68,7 @@ namespace MyBucks.Core.ApiGateway.ApiClient
             };
             var result = await _baseUrl
                 .AppendPathSegment("tokens")
+				.WithHeaders(_headers)
                 .WithBasicAuth(_tokenAuthenticationCredentials.ClientId, _tokenAuthenticationCredentials.ClientSecret)
                 .PostJsonAsync(accountModel)
                 .ReceiveJson<BearerToken>();
